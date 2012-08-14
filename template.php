077728f984220f8c1657648b54a82095e868f33c
Adnan Asani <adnan@nindev.se>
2012-08-14 15:36:13 +0200

    added a page_alter

--- template.php
+++ template.php
@@ -1,5 +1,18 @@
 <?php
 
+function montic_page_alter(&$page) {
+  // Remove all the region wrappers.
+  foreach (element_children($page) as $key => $region) {
+    if (!empty($page[$region]['#theme_wrappers'])) {
+      $page[$region]['#theme_wrappers'] = array_diff($page[$region]['#theme_wrappers'], array('region'));
+    }
+  }
+  // Remove the wrapper from the main content block.
+  if (!empty($page['content']['system_main'])) {
+    $page['content']['system_main']['#theme_wrappers'] = array_diff($page['content']['system_main']['#theme_wrappers'], array('block'));
+  }
+}
+
 function montic_html_head_alter(&$head_elements) {
   $head_elements['system_meta_content_type']['#attributes'] = array(
     'charset' => 'utf-8'
